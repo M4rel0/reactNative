1a41b99fcf9947a6dc43f2251a51ea942275efdd
MarceloLuciani <marcelosluciani@gmail.com>
2025-03-31 22:24:02 -0300

    tentando fazer uma inershadow

--- yourdogApp/src/components/pesquisa/index.tsx
+++ yourdogApp/src/components/pesquisa/index.tsx
@@ -1,14 +1,23 @@
+import React from "react";
 import { Text, View, TextInput } from "react-native";
 import { styles } from "./styles";
+import { LinearGradient } from "expo-linear-gradient";
 
 export function Pesquisa() {
   return (
     <View style={styles.pesquisa}>
-      <TextInput
-        style={styles.pesquisaInput}
-        placeholder="Pesquisar..."
-        placeholderTextColor="#888"
-      />
+      <View style={styles.innerShadow}>
+        <LinearGradient
+          colors={["rgba(0, 0, 0, 0.2)", "rgba(0, 0, 0, 0)"]}
+          style={styles.gradientOverlay}
+        />
+        <TextInput
+          style={styles.pesquisaInput}
+          placeholder="Pesquisar..."
+          placeholderTextColor="#888"
+        />
+      </View>
+      <View style={styles.filtros}></View>
     </View>
   );
 }
